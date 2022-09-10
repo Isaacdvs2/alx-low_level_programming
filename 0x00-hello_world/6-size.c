de278b833b9b79ffe1cf05dccadbd80e1886e664
Isaacdvs2 <dvs2mona@gmail.com>
2022-09-10 05:44:32 -0700

    add c file 6 sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of our program
+ * @void - void method.
+ *
+ * Description: print size of int long, char and float
+ * Return: return 0 for success
+ */
+
+int main(void)
+{
+	printf("The size of a char:%i byte(s)\n", sizeof(char));
+	printf("The size of an int:%i byte(s)\n", sizeof(int));
+	printf("The size of a long int:%i byte(s)\n", sizeof(long int));
+	printf("The size of a long long int:%i byte(s)\n", sizeof(long long int));
+	printf("The size of a float:%i byte(s)\n", sizeof(float));
+	return (0);
+}
