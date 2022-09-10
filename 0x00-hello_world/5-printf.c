a7f4337595c11939e24226b10c8f55577e706d26
Isaacdvs2 <dvs2mona@gmail.com>
2022-09-10 05:20:20 -0700

    add 5th c file use printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main - entrypoint of a c program
+ * @void - indicates that our method returns no value
+ *
+ * Description: use printf to stdout a string
+ * Return - returns 0 for success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,",\n);
+	return (0);
+}
