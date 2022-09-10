6739f74e0e151342f6a88f0ab34fa30ac685a6f8
Isaacdvs2 <dvs2mona@gmail.com>
2022-09-10 06:50:48 -0700

    add unistd c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - entry point of our program
+ * @void - voids our main to return no value
+ *
+ * Description: write to stdout without using any stdio.h functions
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (0);
+}
