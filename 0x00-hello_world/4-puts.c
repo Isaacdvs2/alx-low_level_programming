8f1c48a2033b5af497f19d7b1a601b46bbd6cd1a
Isaacdvs2 <dvs2mona@gmail.com>
2022-09-10 05:10:15 -0700

    debugged c file 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+
 /*
  * main - main function to run the program.
  * @void - void parameter indicates tht the main func returns no value
  *
- * Description: This program accepts a string and uses the puts function.
+ * Description: main uses the puts function.
  * Return: returns 0 for success
-* */
+*/
 
-int main(void){
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
