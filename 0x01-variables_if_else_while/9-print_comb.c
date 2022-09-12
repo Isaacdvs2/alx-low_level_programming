aa982bbe9e29771436b28ff347e28d675db06846
Isaacdvs2 <dvs2mona@gmail.com>
2022-09-11 19:59:25 -0700

    add prog to print single dig nums

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of a c prog.
+ * @void - allows main to retirn no value.
+ *
+ * Description: a prog to print single combinations of
+ * single digit nums.
+ * Return: return 0 for success
+ */
+
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+
+		if (i != 9)
+		{
+			!putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
