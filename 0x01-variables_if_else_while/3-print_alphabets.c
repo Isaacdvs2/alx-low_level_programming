a30481ad66189f18483ae7721d3e75ea8346b988
Isaacdvs2 <dvs2mona@gmail.com>
2022-09-11 18:34:44 -0700

    print alphaBet source

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point of the c program.
+ * @void - allowws the main method to return no value
+ *
+ * Description: print the alphabet using putchar in lower
+ * case followed by Upper case. Use the
+ * ASCII codes to achieve the purpose.
+ * Return: return 0 for sucess
+ */
+
+int main(void)
+{
+	char ch;
+	char i;
+
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+
+	for (i = 65; i <= 97; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
