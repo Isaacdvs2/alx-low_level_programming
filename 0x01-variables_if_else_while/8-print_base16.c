2525198ed0827bdc8395240dd086957f277d524b
Isaacdvs2 <dvs2mona@gmail.com>
2022-09-11 19:30:33 -0700

    add correct file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main -  entry method of a c program.
+ * @void - allows main to return no value.
+ *
+ * Description: a prog to print the nums of base 16
+ * in lower case.
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+
+	for (i = 0; i <= 102; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
