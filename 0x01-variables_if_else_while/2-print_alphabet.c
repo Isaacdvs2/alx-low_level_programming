96a3328ad17b9d88bcaa103a14d2da651d450c2f
Isaacdvs2 <dvs2mona@gmail.com>
2022-09-11 18:10:48 -0700

    add print_alpha source

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point of the c program.
+ * @void - allowws the main method to return no value
+ *
+ * Description: print the alphabet using putchar. Use the
+ * ASCII codes to achieve the purpose.
+ * Return: return 0 for sucess
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	putchar("\n");
+	return (0);
+}
