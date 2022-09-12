663dbe2380a822cb484c6bf2588895d63a6bf5b7
Isaacdvs2 <dvs2mona@gmail.com>
2022-09-11 19:03:09 -0700

    add prog to print base 10 nums

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - the entry method of a c program.
+ * @void - allows the main method to return no value.
+ *
+ * Description: A program to print single digits of base 10.
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%i");
+	}
+	printf("\n");
+	return (0);
+}
