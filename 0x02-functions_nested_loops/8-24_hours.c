370164fa8a5612454670ab4945d64ef9928407f5
Isaacdvs2 <dvs2mona@gmail.com>
2022-09-14 18:08:18 -0700

    debugged output errors

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -22,12 +22,21 @@ for (i = 0; i <= 2; i++)
 		{
 			for (l = 0; l <= 9; l++)
 			{
-				_putchar(i + '0');
-				_putchar(j + '0');
+				a = i + '0';
+				b = j + '0';
+				c = k + '0';
+				d = l + '0';
+				_putchar(a);
+				_putchar(b);
 				_putchar(':');
-				_putchar(k + '0');
-				_putchar(l + '0');
+				_putchar(c);
+				_putchar(d);
 				_putchar('\n');
+
+				if (a == 2 && b == 3 && c == 5 && d == 9)
+				{
+					break;
+				}
 			}
 		}
 	}
