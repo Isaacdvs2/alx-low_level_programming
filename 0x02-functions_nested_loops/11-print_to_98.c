3cac88f8950c3c0927f59d1768eebd9505d7c3fb
Isaacdvs2 <dvs2mona@gmail.com>
2022-09-14 20:10:58 -0700

    add print to 98 prog

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - a method to print to 98.
+ * @n: the number to count to 98.
+ *
+ * Description: a prog to print from n to
+ * 98. 
+ * Return: no return val
+ * */
+
+void print_to_98(int n)
+{
+
+	while (n < 98)
+	{
+			printf("%i, ", n);
+				n++;
+	}
+	while (n > 98)
+	{
+			printf("%i, ", n);
+				n--;
+	}
+
+	printf("98");
+	putchar('\n');
+}
