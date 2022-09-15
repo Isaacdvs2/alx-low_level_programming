65e6fb351e06afbfe8d8aacfdc1cce510d4e7f05
Isaacdvs2 <dvs2mona@gmail.com>
2022-09-14 20:04:52 -0700

    add 10 add prog and files

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - method to add two integers
+ * @a: the first int to add.
+ * @b: the second int.
+ *
+ * Description: a prog that add two ints together.
+ * Return: return the result
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+return (result);
+}
---
+++ 0x02-functions_nested_loops/10-test.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+ int n;
+
+ n = add(100, 20);
+ printf("%d\n", n);
+ return (0);
+}
