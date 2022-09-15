8938503cd9725670e83a9cd5c1365f4fe04691ef
Isaacdvs2 <dvs2mona@gmail.com>
2022-09-14 17:43:54 -0700

    add 8 24hr prog and .h file

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - a void method to print 24hr time.
+ * @void: lets our method return no value.
+ *
+ * Description: a prog to print every minute of the
+ * day from 0.00 -> 23:59. _putcharonly is allowed.
+ * Return: no value is returned
+ */
+
+void jack_bauer(void)
+{
+int i, j, k, l;
+
+for (i = 0; i <= 2; i++)
+{
+	for (j = 0; j <= 3; j++)
+	{
+		for (k = 0; k <= 5; k++)
+			for (l = 0; l <= 9; l++)
+			{
+				_putchar(i + '0');
+				_putchar(j + '0');
+				_putchar(':');
+				_putchar(k + '0');
+				_putchar(l + '0');
+
+				if (i + k + j + l == 19)
+				{
+					break;
+				}
+			}
+	}
+}
