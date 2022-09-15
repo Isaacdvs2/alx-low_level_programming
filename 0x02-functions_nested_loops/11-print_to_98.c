e27cd135c118d96815c81b33f0b3b5be2dd339ef
Isaacdvs2 <dvs2mona@gmail.com>
2022-09-14 20:16:45 -0700

    betty doc

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,24 +6,22 @@
  * @n: the number to count to 98.
  *
  * Description: a prog to print from n to
- * 98. 
+ * 98.
  * Return: no return val
- * */
+*/
 
 void print_to_98(int n)
 {
-
 	while (n < 98)
 	{
-			printf("%i, ", n);
-				n++;
+		printf("%i, ", n);
+		n++;
 	}
 	while (n > 98)
 	{
-			printf("%i, ", n);
-				n--;
+		printf("%i, ", n);
+		n--;
 	}
-
 	printf("98");
 	putchar('\n');
 }
