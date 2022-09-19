5fa58fb5fbf3c73c8573470b38ca979655029b98
Isaacdvs2 <dvs2mona@gmail.com>
2022-09-18 18:18:01 -0700

    add fizzbuzz prog

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * main - an entry method of a c program.
+ * @void: takes no arguments.
+ *
+ * Deascription: a prog to print nums 1 to 100. For
+ * a num that is a multiple of 3, print Fizz, For a
+ * multiple of 5, print Buzz. For a multiple of both
+ * print FizzBuzz.
+ * Return: 0 for success.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if ((i % 5 == 0) && (i % 3 == 0))
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
+
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+			printf(" ");
+		}
+
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+		else
+		{
+			printf("%i", i);
+			printf(" ");
+		}
+	}
+	printf("\n");
+return (0);
+}
