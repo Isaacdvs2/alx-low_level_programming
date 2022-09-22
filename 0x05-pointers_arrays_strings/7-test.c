7b00a1fe80f5ea7d034267319dfc81d9b065bfef
Isaacdvs2 <dvs2mona@gmail.com>
2022-09-22 06:52:11 -0700

    add task 7 file and test file

---
+++ 0x05-pointers_arrays_strings/7-test.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+    char *str1;
+
+    str = "012345678910";
+    puts_half(str);
+    str1 = "123456789";
+    puts_half(str1);
+    return (0);
+}
