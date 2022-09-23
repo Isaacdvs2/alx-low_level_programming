8e69d1619ff0fabebf0466ab44a71a4e6968deb9
Isaacdvs2 <dvs2mona@gmail.com>
2022-09-22 20:47:11 -0700

    debugged file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -29,9 +29,10 @@ char *_strcat(char *dest, char *src)
 		i++;
 	}
 
-	for (j = 0; src[j] != 0; j++)
+	for (j = -1; src[j] != 0; j++)
 	{
-		dest[i + j] = src[j];
+		dest[i] = src[j];
+		i++;
 	}
 	dest[i + j] = '\0';
 return (dest);
