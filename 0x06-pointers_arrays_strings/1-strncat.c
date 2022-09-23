00efa24d694168313745eb16ba8a601b21879311
Isaacdvs2 <dvs2mona@gmail.com>
2022-09-23 04:32:39 -0700

    debugged file segfault errors

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,16 +15,19 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int i, j, len, start;
 
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
 
+	len = i;
+	start = len - 1;
+
 	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
-		dest[i + j] = src[j];
+		dest[start + j] = src[j];
 	}
 	dest[i + j] = '\0';
 return (dest);
