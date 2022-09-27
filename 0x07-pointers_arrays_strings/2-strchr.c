12d1069dd6bb44b601fa355d45d3fd4b23b0439f
Isaacdvs2 <dvs2mona@gmail.com>
2022-09-26 19:11:02 -0700

    add strchr prog and updated header file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strchr - check for the occurence of a char.
+ * @s: the string to check for char.
+ * @c: the char to check in s.
+ * Description: a function to check for the first
+ * occurance of a character. The function
+ * returns the first occurance of c in s, or else
+ * returns null.
+ * Return: null if c not in s.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; c != '\0' && s[i] <= '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+return ('\0');
+}
