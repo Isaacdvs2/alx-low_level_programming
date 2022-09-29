c1f1d3ecf4f7cf0b3f4077b07c77d3ec9e0395fe
Isaacdvs2 <dvs2mona@gmail.com>
2022-09-29 06:50:03 -0700

    added sqrt prog, updated header file

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _int_sqrt_recursion - a function to compute the sqrt of n.
+ * @n: the num to compute the sqrt for.
+ *
+ * Description: a prog that computes the sqrt of n using
+ * recursion.
+ * Return: the sqrt of n as an int. if n does not have a natural
+ * sqrt, return -1.
+ */
+
+int _sqrt_recursion(int n)
+{
+return (square(n, 1);
+}
+
+/**
+ * square - a function to compute the square of n.
+ * @n: the number to find the square of.
+ * @rt:the sqrt.
+ *
+ * Description: the square function computes the square of n
+ * using recursion.
+ * Return: the square of n as int.
+ */
+
+
+int square(int n, int rt)
+{
+	if (rt * rt == n)
+	{
+		return (n);
+	}
+
+	else if (rt * rt < n)
+	{
+		return (square(n, rt +1));
+	}
+
+	else
+	{
+		return (-1);
+	}
+}
