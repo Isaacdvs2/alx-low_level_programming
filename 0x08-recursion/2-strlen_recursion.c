c7e640764e876e7359c89e44951aeec9d8254c44
Isaacdvs2 <dvs2mona@gmail.com>
2022-09-29 06:10:56 -0700

    add strlen rec func and updated .h file

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - a function to return the length of a string.
+ * @s: the string to compute the length of.
+ *
+ * Description: a prog that computes the length of the string "s"
+ * using recursion technique.
+ * Return: the length of the string as an integer.
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i;
+	
+	if (*s != '\0')
+	{
+		i += _strlen_recursion(s + 1) + 1;
+	}
+	return (i);
+}
