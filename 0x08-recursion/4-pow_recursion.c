a92d0d22375fe8dd6d342e0baf1152a06609b51b
Isaacdvs2 <dvs2mona@gmail.com>
2022-09-29 06:30:32 -0700

    add pow_rec prog, updated header file

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - a function to compute the power of a number.
+ * @x: the base number.
+ * @y: the power.
+ *
+ * Description: a prog that returns the value of x raised to the
+ * power y.
+ * Return: returns the value of x ^ y. If y < 0, return -1.
+ */
+
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	else if (y == 0)
+	{
+		return (x);
+	}
+
+	else
+	{
+		return(x * _pow_recursion(x, y - 1));
+	}
+}
