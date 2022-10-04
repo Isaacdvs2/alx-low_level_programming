df036bac2ac4441cb34a13f353d68bf331894e0a
Isaacdvs2 <dvs2mona@gmail.com>
2022-10-04 01:12:39 -0700

    add args prog, updated header file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main -  is the entry point of a c program.
+ * @argc: represent the number of arguments.
+ * @argv: represent a pointer to pointers of arrays
+ * which represent the actual arguments.
+ *
+ * Description: a program that writes its name to std out
+ * followed by a new line. We know that that for argc > 1,
+ * argv[0] is the name of the program and argv[argc] is the
+ * null pointer. anything in between 0 and the null pointer
+ * is the actual arguments.
+ * Return: return 0 for success.
+ */
+
+int main(int argc, char *argv[])
+{
+	while (argc > 1)
+	{
+		_putchar(argv[0]);
+	}
+	_putchar('\n');
+return (0);
+}
