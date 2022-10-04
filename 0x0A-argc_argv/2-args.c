0ab9f70dad4bee4222cc11c3f08c4cf5090bc9df
Isaacdvs2 <dvs2mona@gmail.com>
2022-10-04 02:39:37 -0700

    added print args prog and test files

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point of a c program.
+ * @argc: represents the num of args to main.
+ * @argv: a pointer of pointers to arrays which
+ * represents the arguments thmeselves
+ *
+ * Description: a prog that displays the arguments
+ * passed to the program.
+ * Return: 0 for success.
+ */
+
+int main(int argc, char *argv[])
+{
+	while(argc--)
+		printf("%s\n", *argv++);
+return (0);
+}
