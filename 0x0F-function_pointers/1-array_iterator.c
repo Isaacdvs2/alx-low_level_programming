5d65f6c0ffa1db6aa953283767d6c469a29fb5ce
Isaacdvs2 <dvs2mona@gmail.com>
2022-10-12 18:12:47 -0700

    added array iter prog

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "function_pointers.c"
+
+/**
+ * array_iterator - a function to iterate an array.
+ * @array: is the array to apply the function on.
+ * @size: the size of the array.
+ * @action: is the function to call-back that will
+ * act on the array. "action" is the pointer to the function.
+ *
+ * Description: a prog that calls back a function and
+ * executes that function on an array of size size.
+ * Return: void, returns no data.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+	
+	if (array != NULL && action != NULL)
+	{
+        	for (i = 0; i < size; i++)
+                	action(array[i]);
+	}
+}
