e2b57f1f67febb4164294e43b71076ff6abaf71f
Isaacdvs2 <dvs2mona@gmail.com>
2022-10-21 01:24:03 -0700

    added task 2 prog and all files

--- 0x12-singly_linked_lists/create_node.c
+++ 0x12-singly_linked_lists/create_node.c
@@ -12,29 +12,27 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
-	int len_str;
+	const char *temp_str;
+	list_t new_node; /* create empty node of type list_t */
 
-	new_node = (list_t *) malloc(sizeof(list_t));
-
+	new_node = newNode();
+	temp_str = str;
+	
 	if (new_node == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
+	if (head == NULL || head)
 	{
-		free(new_node);
-		return (NULL);
+		if (new_node -> str == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		new_node -> str = strdup(temp_str);
+		new_node -> next = *head;
+		*head = new_node;
 	}
-	for (len_str = 0; str[len_str] != '\0';  len_str++)
-	;
-
-	new_node->len = len_str;
-	new_node->next = *head;
-	*head = new_node;
-
-	return (new_node);
+return (new_node);
 }
